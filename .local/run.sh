e1135facf3968843fa68a3dd59c8cca9d4cb1249
EdzonAG <edzon_ag@outlook.com>
2022-11-13 01:46:57 -0600

    November 13

---
+++ .local/run.sh
@@ -0,0 +1,3 @@
+#! /bin/bash
+
+cd ~/Coding/MATLAB && matlab -desktop
---
+++ .local/tv.sh
@@ -0,0 +1,3 @@
+#! /bin/bash
+
+xrandr --output HDMI-A-0 --set underscan on --set "underscan vborder" 40 --set "underscan hborder" 72
